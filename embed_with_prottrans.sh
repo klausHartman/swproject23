a286ed3cc225ec1ff3474a16b2fa0736422b292d
klausHartman <kh_athome@gmx.net>
2024-02-04 21:59:12 +0100

    shebang corrected in .sh file

--- embed_with_prottrans.sh
+++ embed_with_prottrans.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 python prott5_embedder.py --input sequences/some.fasta --output embeddings/residue_embeddings.h5
 python prott5_embedder.py --input sequences/some.fasta --output embeddings/protein_embeddings.h5 --per_protein 1
